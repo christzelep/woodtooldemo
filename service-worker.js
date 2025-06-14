a0f6ab00ead6afe2dd34f24ff2f349f2a54f6268
christzelep <trendinggadgets23@gmail.com>
2025-06-14 16:23:17 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,18 @@
-const CACHE_NAME = 'woodworking-assistant-cache-v1';
+const CACHE_NAME = 'woodworking-assistant-cache-v2'; // Αλλαγή έκδοσης για cache busting
 const urlsToCache = [
-    './',
-    './index.html',
-    './manifest.json',
-    './icons/icon-192.png',
-    './icons/icon-512.png'
+    '/',
+    '/index.html',
+    '/manifest.json',
+    'icons/download-icon.png',
+    'icons/icon-192.png',
+    'icons/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then((cache) => {
+            .then(cache => {
+                console.log('Caching assets');
                 return cache.addAll(urlsToCache);
             })
     );
@@ -18,10 +20,11 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('activate', (event) => {
     event.waitUntil(
-        caches.keys().then((cacheNames) => 
+        caches.keys().then(cacheNames => 
             Promise.all(
-                cacheNames.map((cacheName) => {
+                cacheNames.map(cacheName => {
                     if (cacheName !== CACHE_NAME) {
+                        console.log('Deleting old cache:', cacheName);
                         return caches.delete(cacheName);
                     }
                 })
@@ -33,8 +36,31 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
-            .then((response) => {
-                return response || fetch(event.request);
+            .then(response => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                
+                // Clone the request
+                const fetchRequest = event.request.clone();
+                
+                return fetch(fetchRequest).then(response => {
+                    // Check if valid response
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+                    
+                    // Clone the response
+                    const responseToCache = response.clone();
+                    
+                    caches.open(CACHE_NAME)
+                        .then(cache => {
+                            cache.put(event.request, responseToCache);
+                        });
+                    
+                    return response;
+                });
             })
     );
 });
